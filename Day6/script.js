e8942659aa8b7f4f2d5ccc0fb982171bbd1b1f47
Vuong Nguyen <vuong.it@MacBook-Pro-cua-Vuong.local>
2023-09-18 21:21:08 +0700

    Day6 - edit checkpoint

--- Day6/script.js
+++ Day6/script.js
@@ -4,9 +4,11 @@ for (let i = 0; i < 200; i++) {
     document.body.innerHTML += '<div class="item">Content</div>'
 }
 const handleScroll = () => {
+    const firstItemBound = items[0].getBoundingClientRect()
+    const boxHeight = firstItemBound.bottom - firstItemBound.top -8
     for (const item of items) {
         const bounding = item.getBoundingClientRect()
-        const checkpoint = window.innerHeight / 5 * 4
+        const checkpoint = window.innerHeight - boxHeight
         if (bounding.top < checkpoint) {
             item.classList.add('active')
         } else {
